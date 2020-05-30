039128ec6c4f1662d95bd5d634a23c345bdf799d
VadimRijiiBGU <nikolskivadim@gmail.com>
2020-05-30 21:35:31 +0300

    feat: added support to sent mesd message via sockets

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,15 +1,46 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import openSocket from 'socket.io-client';
 import './styles.scss';
 
 const Home = () => {
+    const socket = openSocket('http://localhost:3020');
+    const [message, setMessage] = useState('');
+
+    socket.on('chat-message', message => {
+        const element = document.createElement('div');
+        element.innerText = message;
+
+        const messageArea = document.getElementById('message-area');
+        if (!messageArea) {
+            return;
+        }
+
+        messageArea.appendChild(element);
+    });
+
+    const sendMessage = (e) => {
+        e.preventDefault();
+        socket.emit('send-chat-message', message);
+        setMessage('');
+    };
+
     return (
         <div className="home-page-wrapper">
             <div className="send-message-container">
-                <input placeholder="Write you message here" className="send-message-input"/>
-                <button className="send-message-button">Send message</button>
+                <input
+                    className="send-message-input"
+                    placeholder="Write you message here"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                />
+                <button
+                    className="send-message-button"
+                    onClick={sendMessage}
+                >
+                    Send message
+                </button>
             </div>
-            <div className="hello-to-chat-message">
+            <div id="message-area" className="hello-to-chat-message">
                 Hello to our chat =)
             </div>
         </div>
